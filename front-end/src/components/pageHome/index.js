01c1efc7450f958cd16537e0d8d413ea4c968e9f
Francis <francis.munabeno@yahoo.fr>
2020-10-09 13:27:45 +0200

    add reset

--- front-end/src/components/pageHome/index.js
+++ front-end/src/components/pageHome/index.js
@@ -5,8 +5,22 @@ class Home extends React.Component{
     return(
       <div>
         <h1>Home</h1>
+        <section>
+          <div id="navHome">
+            <ul>
+              <li>Femme</li>
+              <li>Homme</li>
+            </ul>
+          </div>
+          <div id="card">
+            <img src="" alt="article" />
+            <p>Jean Skinny bleu</p>
+            <p>49,99 $</p>
+            <button>Buy Now</button>
+          </div>
+        </section>
       </div>
     )
   }
-}
+} 
 export default Home;
