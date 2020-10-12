2c0fa1a84f495ee0aae6056b94c1674a236cd89d
Francis <francis.munabeno@yahoo.fr>
2020-10-12 15:24:20 +0200

    page home done with video and svg animat

--- front-end/src/components/pageAccueil/index.js
+++ front-end/src/components/pageAccueil/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import './style.css'
+import video from '../../asset/videos/pub3.mp4'
 class Accueil extends React.Component{
   render(){
     return(
-      <div>
-        <h1>Accueil</h1>
+      <div id="body_Accueil">
+        <video autoplay muted>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div id="link_Home">
+          <div id="svg">
+            <a>HOME</a>
+            <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg" >
+              <rect class="stroke" height="60" width="320" />
+              <div>zaeazeae</div>
+            </svg>
+          </div>
+        </div>
       </div>
     )
   }
