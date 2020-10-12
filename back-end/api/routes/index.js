1caed7df246e4cd5779580b010b0d3124a5e1b6c
Mowafag <mmowfug@yahoo.com>
2020-10-12 15:40:51 +0200

    signup

---
+++ back-end/api/routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../database')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+router.post('/sign-up', (req, res) => {
+  const {fname, lname, email} = req.body 
+  const pass = bcrypt.hashSync(req.body.password, 10)
+  const sql = `INSERT INTO users (first_name, last_name, email, password) VALUES ('${fname}', '${lname}', '${email}','${pass}')`
+
+  db.query(`SELECT * FROM users WHERE email = '${email}'`, (err, results) => {
+    if (err) throw err
+
+    if(results.length){
+      res.status(400).send('This email already registered !')
+    } else {
+      db.query(sql, (err) => { res.status(200).send('successfully registered') })
+    }  
+  })
+})
+
+.post('/sign-in', (req, res) => {
+  const {email, pass} = req.body
+  
+  db.query(`SELECT * FROM users WHERE email = '${email}'`, (err, results) => {
+    if (err) throw err
+
+    if(results[0] && bcrypt.compareSync(pass, results[0].password)){
+      let token = jwt.sign({userId: results[0].id, email: results[0].email, userName: results[0].name}, 'usersecret')
+      res.status(201).send({auth: true, token: token}) 
+    } else if(!results[0]) {
+      res.status(400).send({hasAccount: false ,msg: "Sorry, this user isn't recognized"})
+    } else {
+      res.status(400).send({hasAccount: true, msg: "Wrong password, try again !"})
+    } 
+  })
+})
+
+module.exports = router
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // LAYOUT 
 import Header from './components/Layout/header/nav'
@@ -7,6 +8,7 @@ import Header from './components/Layout/header/nav'
 // COMPONENTS
 import Accueil from './components/pageAccueil'
 import Home from './components/pageHome'
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
           </Route>
           <Route exact path="/Home">
             <Home/>
+          </Route>
+          <Route exact path="/SignUp">
+            <SignUp />
           </Route>
         </Switch>
       </Router>
