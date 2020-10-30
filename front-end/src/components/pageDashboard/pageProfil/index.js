e54d7ba7f24a3b91bfdfefe063a376d763589661
Mowafag <mmowfug@yahoo.com>
2020-10-30 11:42:06 +0100

    profile1

--- front-end/src/components/pageDashboard/pageProfil/index.js
+++ front-end/src/components/pageDashboard/pageProfil/index.js
@@ -52,7 +52,7 @@ class Profil extends React.Component{
       console.log(res.data)
     })
     .catch(err => {
-      console.log(err.response)
+      console.log(err.response.data)
     })
     Object.keys(this.state).forEach(elm => this.setState({[elm]: ''}))
   }
